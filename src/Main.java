476e1ec146b0a5d23ba31c30467fbb82501d6d30
diirector <jozef.kubikml@gmail.com>
2019-02-27 14:11:48 +0100

    No features were added!

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Github, I am here!");
+        System.out.println("But in fact, I do not know what to do here :(");
     }
 }
